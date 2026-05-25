4b49216d43b7e72b07ca34cad10ee16db089855a
SuperMan8743 <thedevdaydm@gmail.com>
2026-05-25 22:26:26 +0530

    working on nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,8 +4,11 @@ function Navbar() {
   return (
     <>
       <nav>
-        <div>
+        <div  className='flex'>
           <ul>
+            <li><a href="logo">logo</a></li>
+          </ul>
+          <ul  className='flex gap-1.5'>
             <li><a href="Home">Home</a></li>
             <li><a href="About">About</a></li>
             <li><a href="Contact">Contact</a></li>
