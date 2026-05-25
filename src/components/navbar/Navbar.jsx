90a492bd86be131812ea501b0d6c8bdafb73a1d0
SuperMan8743 <thedevdaydm@gmail.com>
2026-05-25 22:16:54 +0530

    working on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav>
+        <div>
+          <ul>
+            <li><a href="Home">Home</a></li>
+            <li><a href="About">About</a></li>
+            <li><a href="Contact">Contact</a></li>
+            <li><a href="Project">Project</a></li>
+            
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 
