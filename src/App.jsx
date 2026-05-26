333ae6fec9b9d6edbcadd78464c200b859147ff3
SuperMan8743 <thedevdaydm@gmail.com>
2026-05-26 11:26:34 +0530

    uupdaing this company laptop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import Homepage from './pages/Homepage'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Homepage/>
+    <Navbar/>
+  
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <div>
+      <ul>
+        <li><a href="home">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="project">Project</a></li>
+        <li><a href="contact">Contact</a></li>
+      </ul>
+     </div>
+    </>
   )
 }
 
