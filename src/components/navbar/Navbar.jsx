fae3a319e59b03a1aabd35e75a2bf9f3bca12d42
SuperMan8743 <thedevdaydm@gmail.com>
2026-05-28 11:10:57 +0530

    new clone working again

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-      <nav className='flex'>
+      <nav >
         <div><a href="logo basis=30">logo</a></div>
         <div className=' basis-70 flex flex-row-reverse'>
           <ul className='flex flex-row'>
-            <li><a href="home">Home</a></li>
-            <li><a href="about">About US</a></li>
+            <li className=''><a href="home">Home</a></li>
+            <li className=''><a href="about">About US</a></li>
             <li><a href="project">Projects</a></li>
             <li><a href="contactus">Contact Us</a></li>
           </ul>
