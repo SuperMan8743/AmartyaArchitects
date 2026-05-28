308347baa5c089c29347eec24c11da158ae9bb19
SuperMan8743 <thedevdaydm@gmail.com>
2026-05-28 11:20:16 +0530

    working on navbar position setup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-      <nav >
+      <nav className='flex flex-row justify-between '>
         <div><a href="logo basis=30">logo</a></div>
-        <div className=' basis-70 flex flex-row-reverse'>
-          <ul className='flex flex-row'>
+        <div >
+          <ul className='flex flex-row gap-10'>
             <li className=''><a href="home">Home</a></li>
             <li className=''><a href="about">About US</a></li>
             <li><a href="project">Projects</a></li>
