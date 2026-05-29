5945a1edd9310d5b71912b69976b48c85658a594
SuperMan8743 <thedevdaydm@gmail.com>
2026-05-29 19:34:45 +0530

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-   <footer className='flex justify-between border-2 flex-wrap'>
-    <section className='border-2 basis-full'>1</section>
-    <section className='border-2 basis-50 md:basis-full'>1</section>
-    <section className='border-2 basis-50 md:basis-full'>1</section>
-  
-   </footer>
-  )
+    <footer
+      className=" min-h-[400px] bg-fill bg-center bg-no-repeat
+  bg-[url('/footer.jpg')] flex
+    flex-col
+    justify-center
+    items-center"
+    >
+      <ul className="text-center text-white py-auto bold">
+        <li className="text-2xl text-center">Don't Wait </li>
+        <li className="text-5xl">Start Buidling Today</li>
+      </ul>
+      <nav className="flex flex-row justify-center">
+        <section >
+          <ul className="flex flex-row gap-4 mt-6  text-white">
+
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/project">Projects</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+
+          </ul>
+        </section>
+        <section></section>
+      </nav>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
